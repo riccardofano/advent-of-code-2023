e935dd29262449bb7db996f896700596f9291517
Riccardo Fano <rcr.fano@gmail.com>
2023-12-02 13:58:25 +0100

    Solve part 1 of day 2

---
+++ src/bin/02.rs
@@ -0,0 +1,78 @@
+advent_of_code::solution!(2);
+
+fn is_game_possible(sets: &[&str]) -> bool {
+    for set in sets.iter() {
+        for cube in set.split(", ") {
+            let Some((number, color)) = cube.split_once(' ') else {
+                continue;
+            };
+
+            dbg!(number, color);
+            let number = number.parse::<u32>().unwrap();
+            if (color == "red" && number > 12)
+                || (color == "green" && number > 13)
+                || (color == "blue" && number > 14)
+            {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut ids_sum = 0;
+
+    for game in input.lines() {
+        let (game_id, rest) = game.split_once(": ").unwrap();
+        let id = game_id
+            .strip_prefix("Game ")
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+
+        let sets = rest.split(';').map(|s| s.trim()).collect::<Vec<_>>();
+        if is_game_possible(&sets) {
+            ids_sum += id;
+        }
+    }
+
+    Some(ids_sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut ids_sum = 0;
+
+    for game in input.lines() {
+        let (game_id, rest) = game.split_once(": ").unwrap();
+        let id = game_id
+            .strip_prefix("Game ")
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+
+        let sets = rest.split(';').map(|s| s.trim()).collect::<Vec<_>>();
+        if is_game_possible(&sets) {
+            ids_sum += id;
+        }
+    }
+
+    Some(ids_sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(8));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2286));
+    }
+}
