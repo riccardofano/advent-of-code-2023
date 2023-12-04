e3e19bfdac7afd9065c6be259836f3dc82c7056a
Riccardo Fano <rcr.fano@gmail.com>
2023-12-04 11:47:09 +0100

    Solve part 1 day 4

---
+++ src/bin/04.rs
@@ -0,0 +1,60 @@
+use std::collections::HashSet;
+
+advent_of_code::solution!(4);
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let result = input
+        .trim()
+        .lines()
+        .map(|l| {
+            let (_card_info, numbers) = l.split_once(": ").unwrap();
+
+            let (winning, have) = numbers.split_once(" | ").unwrap();
+            let winning_set = winning
+                .split_whitespace()
+                .map(|n| n.parse())
+                .collect::<Result<HashSet<usize>, _>>()
+                .unwrap();
+
+            let count = have
+                .split_whitespace()
+                .filter_map(|n| n.parse::<usize>().ok())
+                .filter_map(|n| winning_set.get(&n))
+                .count();
+
+            // I'm sure there is a better way but it's coming to me right now
+            let mut sum = 0;
+            for i in 0..count {
+                if i == 0 {
+                    sum += 1;
+                } else {
+                    sum *= 2;
+                }
+            }
+            sum
+        })
+        .sum();
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
