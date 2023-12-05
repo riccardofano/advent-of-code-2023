8f97814889877011b1b02025fdcfa4ca66b17562
Riccardo Fano <rcr.fano@gmail.com>
2023-12-05 13:58:18 +0100

    Don't hardcode maps

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,20 +2,22 @@ advent_of_code::solution!(5);
 
 use std::{collections::HashMap, ops::Range};
 
-fn insert_ranges(data: &str, map: &mut HashMap<Range<usize>, Range<usize>>) {
-    data.lines().for_each(|l| {
-        let [dest_start, source_start, range_len]: [usize; 3] = l
-            .split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
+fn map_range(data: &str) -> Vec<(Range<usize>, Range<usize>)> {
+    data.lines()
+        .map(|l| {
+            let [dest_start, source_start, range_len]: [usize; 3] = l
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
 
-        map.insert(
-            source_start..source_start + range_len,
-            dest_start..dest_start + range_len,
-        );
-    })
+            (
+                source_start..source_start + range_len,
+                dest_start..dest_start + range_len,
+            )
+        })
+        .collect()
 }
 
 fn number_in_available_ranges(
@@ -37,9 +39,11 @@ fn number_in_available_ranges(
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let parts = input.trim().split("\n\n").collect::<Vec<_>>();
+    let mut parts = input.trim().split("\n\n");
 
-    let seeds = parts[0]
+    let seeds = parts
+        .next()
+        .unwrap()
         .split_once(": ")
         .unwrap()
         .1
@@ -47,46 +51,33 @@ pub fn part_one(input: &str) -> Option<usize> {
         .map(|n| n.parse().unwrap())
         .collect::<Vec<usize>>();
 
-    let parts = parts
-        .iter()
-        .skip(1)
-        .map(|p| p.lines().skip(1).collect::<Vec<_>>().join("\n"))
+    let maps = parts
+        .map(|p| {
+            p.lines()
+                .skip(1)
+                .flat_map(map_range)
+                .collect::<HashMap<Range<usize>, Range<usize>>>()
+        })
         .collect::<Vec<_>>();
 
-    let mut seed_to_soil = HashMap::new();
-    insert_ranges(&parts[0], &mut seed_to_soil);
-    let mut soil_to_fertilizer = HashMap::new();
-    insert_ranges(&parts[1], &mut soil_to_fertilizer);
-    let mut fertilizer_to_water = HashMap::new();
-    insert_ranges(&parts[2], &mut fertilizer_to_water);
-    let mut water_to_light = HashMap::new();
-    insert_ranges(&parts[3], &mut water_to_light);
-    let mut light_to_temp = HashMap::new();
-    insert_ranges(&parts[4], &mut light_to_temp);
-    let mut temp_to_humidity = HashMap::new();
-    insert_ranges(&parts[5], &mut temp_to_humidity);
-    let mut humidity_to_location = HashMap::new();
-    insert_ranges(&parts[6], &mut humidity_to_location);
-
-    let lowest_location = seeds
+    let result = seeds
         .iter()
-        .map(|&s| number_in_available_ranges(s, &seed_to_soil).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &soil_to_fertilizer).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &fertilizer_to_water).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &water_to_light).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &light_to_temp).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &temp_to_humidity).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &humidity_to_location).unwrap_or(s))
-        .min()
-        .unwrap();
+        .map(|&s| {
+            maps.iter().fold(s, |res, map| {
+                number_in_available_ranges(res, map).unwrap_or(res)
+            })
+        })
+        .min();
 
-    Some(lowest_location)
+    result
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let parts = input.trim().split("\n\n").collect::<Vec<_>>();
+    let mut parts = input.trim().split("\n\n");
 
-    let seeds = parts[0]
+    let seeds = parts
+        .next()
+        .unwrap()
         .split_once(": ")
         .unwrap()
         .1
@@ -108,40 +99,25 @@ pub fn part_two(input: &str) -> Option<usize> {
         }
     }
 
-    let parts = parts
-        .iter()
-        .skip(1)
-        .map(|p| p.lines().skip(1).collect::<Vec<_>>().join("\n"))
+    let maps = parts
+        .map(|p| {
+            p.lines()
+                .skip(1)
+                .flat_map(map_range)
+                .collect::<HashMap<Range<usize>, Range<usize>>>()
+        })
         .collect::<Vec<_>>();
 
-    let mut seed_to_soil = HashMap::new();
-    insert_ranges(&parts[0], &mut seed_to_soil);
-    let mut soil_to_fertilizer = HashMap::new();
-    insert_ranges(&parts[1], &mut soil_to_fertilizer);
-    let mut fertilizer_to_water = HashMap::new();
-    insert_ranges(&parts[2], &mut fertilizer_to_water);
-    let mut water_to_light = HashMap::new();
-    insert_ranges(&parts[3], &mut water_to_light);
-    let mut light_to_temp = HashMap::new();
-    insert_ranges(&parts[4], &mut light_to_temp);
-    let mut temp_to_humidity = HashMap::new();
-    insert_ranges(&parts[5], &mut temp_to_humidity);
-    let mut humidity_to_location = HashMap::new();
-    insert_ranges(&parts[6], &mut humidity_to_location);
-
-    let lowest_location = seeds
+    let result = seeds
         .iter()
-        .map(|&s| number_in_available_ranges(s, &seed_to_soil).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &soil_to_fertilizer).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &fertilizer_to_water).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &water_to_light).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &light_to_temp).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &temp_to_humidity).unwrap_or(s))
-        .map(|s| number_in_available_ranges(s, &humidity_to_location).unwrap_or(s))
-        .min()
-        .unwrap();
+        .map(|&s| {
+            maps.iter().fold(s, |res, map| {
+                number_in_available_ranges(res, map).unwrap_or(res)
+            })
+        })
+        .min();
 
-    Some(lowest_location)
+    result
 }
 
 #[cfg(test)]
