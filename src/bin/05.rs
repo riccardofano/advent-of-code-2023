b182470ff94840613debe891a0662a7cfaa6e237
Riccardo Fano <rcr.fano@gmail.com>
2023-12-05 12:21:49 +0100

    Brute force part 2 of day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -84,7 +84,64 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let parts = input.trim().split("\n\n").collect::<Vec<_>>();
+
+    let seeds = parts[0]
+        .split_once(": ")
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect::<Vec<usize>>();
+
+    let mut seed_ranges = Vec::new();
+    let mut i = 0;
+    while i < seeds.len() {
+        seed_ranges.push(seeds[i]..seeds[i] + seeds[i + 1]);
+        i += 2;
+    }
+
+    let mut seeds = Vec::new();
+    for range in seed_ranges.iter() {
+        for i in range.start..range.end {
+            seeds.push(i);
+        }
+    }
+
+    let parts = parts
+        .iter()
+        .skip(1)
+        .map(|p| p.lines().skip(1).collect::<Vec<_>>().join("\n"))
+        .collect::<Vec<_>>();
+
+    let mut seed_to_soil = HashMap::new();
+    insert_ranges(&parts[0], &mut seed_to_soil);
+    let mut soil_to_fertilizer = HashMap::new();
+    insert_ranges(&parts[1], &mut soil_to_fertilizer);
+    let mut fertilizer_to_water = HashMap::new();
+    insert_ranges(&parts[2], &mut fertilizer_to_water);
+    let mut water_to_light = HashMap::new();
+    insert_ranges(&parts[3], &mut water_to_light);
+    let mut light_to_temp = HashMap::new();
+    insert_ranges(&parts[4], &mut light_to_temp);
+    let mut temp_to_humidity = HashMap::new();
+    insert_ranges(&parts[5], &mut temp_to_humidity);
+    let mut humidity_to_location = HashMap::new();
+    insert_ranges(&parts[6], &mut humidity_to_location);
+
+    let lowest_location = seeds
+        .iter()
+        .map(|&s| number_in_available_ranges(s, &seed_to_soil).unwrap_or(s))
+        .map(|s| number_in_available_ranges(s, &soil_to_fertilizer).unwrap_or(s))
+        .map(|s| number_in_available_ranges(s, &fertilizer_to_water).unwrap_or(s))
+        .map(|s| number_in_available_ranges(s, &water_to_light).unwrap_or(s))
+        .map(|s| number_in_available_ranges(s, &light_to_temp).unwrap_or(s))
+        .map(|s| number_in_available_ranges(s, &temp_to_humidity).unwrap_or(s))
+        .map(|s| number_in_available_ranges(s, &humidity_to_location).unwrap_or(s))
+        .min()
+        .unwrap();
+
+    Some(lowest_location)
 }
 
 #[cfg(test)]
@@ -100,6 +157,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(46));
     }
 }
