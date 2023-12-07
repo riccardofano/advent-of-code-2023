a1947a2141a87cc95313af6760fcf075dd74d1d6
Riccardo Fano <rcr.fano@gmail.com>
2023-12-07 12:48:27 +0100

    Solve part 2 of day 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,21 +2,36 @@ use std::{collections::HashMap, ops::Deref};
 
 advent_of_code::solution!(7);
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Hash)]
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 enum Card {
-    Ace,
-    King,
-    Queen,
-    Jack,
-    Ten,
-    Nine,
-    Eight,
-    Seven,
-    Six,
-    Five,
-    Four,
-    Three,
+    Placeholder, // This is here so nothing else is zero so I can set the jack to zero in part two
     Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Card {
+    fn order(self) -> u8 {
+        self as u8
+    }
+
+    fn order_part_two(self) -> u8 {
+        match self {
+            Card::Jack => 0,
+            _ => self.order(),
+        }
+    }
 }
 
 impl From<char> for Card {
@@ -51,13 +66,34 @@ enum HandKind {
     HighCard,
 }
 
-impl From<&[Card]> for HandKind {
-    fn from(cards: &[Card]) -> Self {
-        let map = cards.iter().fold(HashMap::new(), |mut map, curr| {
-            *map.entry(curr).or_insert(0) += 1;
+impl HandKind {
+    fn from_part_one(cards: &[Card]) -> Self {
+        let map: HashMap<Card, usize> = cards.iter().fold(HashMap::new(), |mut map, curr| {
+            *map.entry(*curr).or_insert(0) += 1;
             map
         });
 
+        Self::match_entries(&map)
+    }
+
+    fn from_part_two(cards: &[Card]) -> Self {
+        let mut map: HashMap<Card, usize> = cards.iter().fold(HashMap::new(), |mut map, curr| {
+            *map.entry(*curr).or_insert(0) += 1;
+            map
+        });
+
+        if let Some(jacks) = map.remove(&Card::Jack) {
+            let (card, _) = map
+                .iter()
+                .max_by(|a, b| a.1.cmp(b.1))
+                .unwrap_or((&Card::Ace, &0));
+            *map.entry(*card).or_insert(0) += jacks;
+        }
+
+        Self::match_entries(&map)
+    }
+
+    fn match_entries(map: &HashMap<Card, usize>) -> Self {
         match map.len() {
             5 => Self::HighCard,
             4 => Self::Pair,
@@ -81,32 +117,54 @@ impl From<&[Card]> for HandKind {
     }
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug)]
 struct Hand {
     kind: HandKind,
     cards: Vec<Card>,
+    card_order: Vec<u8>,
     bid: usize,
 }
 
 impl Hand {
-    fn parse(line: &str) -> Self {
+    fn parse(line: &str, is_part_two: bool) -> Self {
         let (cards, bid) = line.split_once(' ').unwrap();
         let cards = cards.chars().map(Card::from).collect::<Vec<Card>>();
-        let kind = HandKind::from(cards.deref());
+
+        let (kind, card_order) = if is_part_two {
+            (
+                HandKind::from_part_two(cards.deref()),
+                cards.iter().map(|c| c.order_part_two()).collect::<Vec<_>>(),
+            )
+        } else {
+            (
+                HandKind::from_part_one(cards.deref()),
+                cards.iter().map(|c| c.order()).collect::<Vec<_>>(),
+            )
+        };
 
         Self {
             kind,
             cards,
+            card_order,
             bid: bid.parse().unwrap(),
         }
     }
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut hands = input.trim().lines().map(Hand::parse).collect::<Vec<Hand>>();
+    let mut hands = input
+        .trim()
+        .lines()
+        .map(|l| Hand::parse(l, false))
+        .collect::<Vec<Hand>>();
 
-    hands.sort();
-    // dbg!(&hands);
+    hands.sort_by(|a, b| {
+        match a.kind.cmp(&b.kind) {
+            core::cmp::Ordering::Equal => {}
+            ord => return ord,
+        }
+        b.card_order.cmp(&a.card_order)
+    });
 
     let result = hands
         .iter()
@@ -119,7 +177,28 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let mut hands = input
+        .trim()
+        .lines()
+        .map(|l| Hand::parse(l, true))
+        .collect::<Vec<Hand>>();
+
+    hands.sort_by(|a, b| {
+        match a.kind.cmp(&b.kind) {
+            core::cmp::Ordering::Equal => {}
+            ord => return ord,
+        }
+        b.card_order.cmp(&a.card_order)
+    });
+
+    let result: usize = hands
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, hand)| (i + 1) * hand.bid)
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -135,6 +214,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
     }
 }
