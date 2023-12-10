a22d9da19289ad026ac7b1db607e282cbb2e00c6
Riccardo Fano <rcr.fano@gmail.com>
2023-12-10 18:56:16 +0100

    Refactor part 2 solution a bit

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -2,6 +2,8 @@ use std::collections::HashSet;
 
 advent_of_code::solution!(10);
 
+type Point = (usize, usize);
+
 fn build_grid(input: &str) -> Vec<Vec<char>> {
     input
         .trim()
@@ -10,7 +12,7 @@ fn build_grid(input: &str) -> Vec<Vec<char>> {
         .collect()
 }
 
-fn find_start(grid: &[Vec<char>]) -> (usize, usize) {
+fn find_start(grid: &[Vec<char>]) -> Point {
     for (i, line) in grid.iter().enumerate() {
         for (j, char) in line.iter().enumerate() {
             if *char == 'S' {
@@ -21,11 +23,11 @@ fn find_start(grid: &[Vec<char>]) -> (usize, usize) {
     panic!("Expected input to have a starting position marked");
 }
 
-fn is_valid_position((x, y): (usize, usize), grid: &[Vec<char>]) -> bool {
+fn is_valid_position((x, y): Point, grid: &[Vec<char>]) -> bool {
     x <= grid[0].len() && y <= grid.len()
 }
 
-fn neighbors((x, y): (usize, usize), grid: &[Vec<char>]) -> Vec<(usize, usize)> {
+fn neighbors((x, y): Point, grid: &[Vec<char>]) -> Vec<Point> {
     [
         (x.wrapping_sub(1), y),
         (x + 1, y),
@@ -37,10 +39,7 @@ fn neighbors((x, y): (usize, usize), grid: &[Vec<char>]) -> Vec<(usize, usize)> 
     .collect()
 }
 
-fn pipe_connections(
-    (x, y): (usize, usize),
-    grid: &[Vec<char>],
-) -> Option<((usize, usize), (usize, usize))> {
+fn pipe_connections((x, y): Point, grid: &[Vec<char>]) -> Option<(Point, Point)> {
     if !is_valid_position((x, y), grid) {
         return None;
     }
@@ -57,7 +56,7 @@ fn pipe_connections(
     }
 }
 
-fn find_loop_pipes(start: (usize, usize), grid: &[Vec<char>]) -> Vec<(usize, usize)> {
+fn find_loop_pipes(start: Point, grid: &[Vec<char>]) -> Vec<Point> {
     let mut current = start;
 
     for neighbor in neighbors(start, grid) {
@@ -89,19 +88,14 @@ fn find_loop_pipes(start: (usize, usize), grid: &[Vec<char>]) -> Vec<(usize, usi
     pipes
 }
 
-pub fn part_one(input: &str) -> Option<usize> {
-    let grid = build_grid(input);
-    let start = find_start(&grid);
-
-    let pipes = find_loop_pipes(start, &grid);
-    Some(pipes.len() / 2)
+fn have_connection((conn_1, conn_2): (Point, Point), to_find: Point) -> bool {
+    conn_1 == to_find || conn_2 == to_find
 }
 
 fn count_inner_tiles(
     grid: &[Vec<char>],
-    pipes: HashSet<(usize, usize)>,
-    first_pipe: (usize, usize),
-    last_pipe: (usize, usize),
+    pipes: HashSet<Point>,
+    start_connections: (Point, Point),
 ) -> usize {
     let mut inner_tiles = 0;
 
@@ -110,16 +104,10 @@ fn count_inner_tiles(
 
         for x in 0..grid[0].len() {
             if let Some(pos) = pipes.get(&(x, y)) {
-                let to_find = (x, y + 1);
-
-                // We don't know what the shape of the starting tile is so just
-                // get the connections it's attached to
-                let contains_prev_pos = match pipe_connections(*pos, grid) {
-                    Some((conn_1, conn_2)) => conn_1 == to_find || conn_2 == to_find,
-                    None => first_pipe == to_find || last_pipe == to_find,
-                };
-
-                if contains_prev_pos {
+                // `pipe_connections` doesn't know what the shape of the starting tile is
+                // so we pass them connections manually now that we know
+                let connections = pipe_connections(*pos, grid).unwrap_or(start_connections);
+                if have_connection(connections, (x, y + 1)) {
                     outside = !outside;
                 }
             } else if !outside {
@@ -131,15 +119,23 @@ fn count_inner_tiles(
     inner_tiles
 }
 
+pub fn part_one(input: &str) -> Option<usize> {
+    let grid = build_grid(input);
+    let start = find_start(&grid);
+
+    let pipes = find_loop_pipes(start, &grid);
+    Some(pipes.len() / 2)
+}
+
 pub fn part_two(input: &str) -> Option<usize> {
     let grid = build_grid(input);
     let start = find_start(&grid);
-    let pipes = find_loop_pipes(start, &grid);
-    let (first_pipe, last_pipe) = (pipes[1], pipes[pipes.len() - 1]);
 
+    let pipes = find_loop_pipes(start, &grid);
+    let start_connections = (pipes[1], pipes[pipes.len() - 1]);
     let pipes_set: HashSet<(usize, usize)> = pipes.into_iter().collect();
 
-    Some(count_inner_tiles(&grid, pipes_set, first_pipe, last_pipe))
+    Some(count_inner_tiles(&grid, pipes_set, start_connections))
 }
 
 #[cfg(test)]
