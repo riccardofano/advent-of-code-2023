3bfbeb2d569f629e086db48afe3cf42e03636318
Riccardo Fano <rcr.fano@gmail.com>
2023-12-11 11:50:09 +0100

    Solve part 2 of day 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -4,13 +4,14 @@ fn galaxy_positions(
     grid: &[Vec<char>],
     empty_cols: &[usize],
     empty_rows: &[usize],
+    factor: usize,
 ) -> Vec<(usize, usize)> {
     let mut galaxies = Vec::new();
     for (y, row) in grid.iter().enumerate() {
         for (x, &col) in row.iter().enumerate() {
             if col == '#' {
-                let x_offset = empty_cols.iter().take_while(|&&c| c < x).count();
-                let y_offset = empty_rows.iter().take_while(|&&r| r < y).count();
+                let x_offset = empty_cols.iter().take_while(|&&c| c < x).count() * factor;
+                let y_offset = empty_rows.iter().take_while(|&&r| r < y).count() * factor;
                 galaxies.push((x + x_offset, y + y_offset));
             }
         }
@@ -18,7 +19,7 @@ fn galaxy_positions(
     galaxies
 }
 
-pub fn part_one(input: &str) -> Option<usize> {
+fn solve(input: &str, factor: usize) -> Option<usize> {
     let grid = input
         .trim()
         .lines()
@@ -32,7 +33,7 @@ pub fn part_one(input: &str) -> Option<usize> {
         .filter(|&y| (0..grid[0].len()).all(|x| grid[x][y] == '.'))
         .collect::<Vec<_>>();
 
-    let galaxies = galaxy_positions(&grid, &empty_cols, &empty_rows);
+    let galaxies = galaxy_positions(&grid, &empty_cols, &empty_rows, factor);
 
     let result: usize = galaxies
         .iter()
@@ -44,8 +45,12 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(result)
 }
 
+pub fn part_one(input: &str) -> Option<usize> {
+    solve(input, 1)
+}
+
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    solve(input, 1_000_000 - 1)
 }
 
 #[cfg(test)]
@@ -60,7 +65,10 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = solve(
+            &advent_of_code::template::read_file("examples", DAY),
+            100 - 1,
+        );
+        assert_eq!(result, Some(8410));
     }
 }
