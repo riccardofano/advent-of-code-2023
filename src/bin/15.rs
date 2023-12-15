5cc9704933b5fdef249f8b50c0a2a1028df3a5ae
Riccardo Fano <rcr.fano@gmail.com>
2023-12-15 13:31:13 +0100

    Solve part 2 of day 15

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,3 +1,5 @@
+use std::array;
+
 advent_of_code::solution!(15);
 
 fn hash(chars: &str) -> usize {
@@ -10,8 +12,46 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(input.trim().split(',').map(hash).sum())
 }
 
+type LensBox<'a> = Vec<(&'a str, usize)>;
+
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let mut boxes: [LensBox; 256] = array::from_fn(|_| Vec::new());
+
+    input
+        .trim()
+        .split(',')
+        .for_each(|instruction| match instruction.split_once('-') {
+            Some((label, _)) => {
+                let lens_box = hash(label);
+                if let Some(index) = boxes[lens_box].iter().position(|lens| lens.0 == label) {
+                    boxes[lens_box].remove(index);
+                }
+            }
+            None => {
+                let (label, focus) = instruction.split_once('=').unwrap();
+                let lens_box = hash(label);
+                let focus: usize = focus.parse().unwrap();
+
+                match boxes[lens_box].iter().position(|lens| lens.0 == label) {
+                    Some(index) => boxes[lens_box][index].1 = focus,
+                    None => boxes[lens_box].push((label, focus)),
+                }
+            }
+        });
+
+    let res: usize = boxes
+        .iter()
+        .enumerate()
+        .map(|(i, lens_box)| {
+            lens_box
+                .iter()
+                .enumerate()
+                .map(|(j, lens)| (i + 1) * (j + 1) * lens.1)
+                .sum::<usize>()
+        })
+        .sum();
+
+    Some(res)
 }
 
 #[cfg(test)]
@@ -27,6 +67,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(145));
     }
 }
